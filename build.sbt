2d419c2fd4c7052c6c78e0c9bbd57994d052ca7c
ERSUCC <isaacbrown5763@gmail.com>
2025-09-07 13:49:03 -0500

    Remove unnecessary headless property

--- build.sbt
+++ build.sbt
@@ -7,7 +7,7 @@ version    := "1.2.0"
 isSnapshot := true
 
 netLogoClassManager := "org.nlogo.extensions.matrix.MatrixExtension"
-netLogoVersion      := "7.0.0-beta2-7e8f7a4"
+netLogoVersion      := "7.0.0-424b50b"
 
 scalaVersion        := "3.7.0"
 Test / scalaSource := baseDirectory.value / "src" / "test"
--- src/test/Tests.scala
+++ src/test/Tests.scala
@@ -1,6 +1,7 @@
 package org.nlogo.extensions.matrix
 
 import java.io.File
+
 import org.nlogo.headless.TestLanguage
 
 object Tests {
@@ -8,6 +9,4 @@ object Tests {
   val testFiles     = testFileNames.map( (f) => (new File(f)).getCanonicalFile )
 }
 
-class Tests extends TestLanguage (Tests.testFiles) {
-  System.setProperty("org.nlogo.preferHeadless", "true")
-}
+class Tests extends TestLanguage(Tests.testFiles)
